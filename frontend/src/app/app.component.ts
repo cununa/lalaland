38b5165c2c4156eed38053840691ded32c79012c
xiu <conceptcube.tv@gmail.com>
2018-12-21 18:52:18 +0900

    update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = 'schedule';
+  rootPage:any = 'main';
 
   constructor(
     platform: Platform,
