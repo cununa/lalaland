df7aac735ae4bd528102a52fd85ccd4f277ab304
sebyul2 <sebyul3@gmail.com>
2018-12-28 16:20:46 +0900

    [server] 에러 헨들러 추

--- backend/route/index.js
+++ backend/route/index.js
@@ -1,8 +1,17 @@
 'use strict'
 
 module.exports = (app) => {
-    app.use(require('./api'))
-    // app.use(require('./test'))
-    // app.use(require('./sample'))
-    // app.use(require('./xiu'))
+  app.use(require('./api'))
+  // app.use(require('./test'))
+  // app.use(require('./sample'))
+  // app.use(require('./xiu'))
+
+  // error handller
+  app.use((err, req, res, next) => {
+    console.log(err)
+    res.json({
+      code: 9999,
+      message: err.message
+    })
+  })
 }
